1db57d74b081e8844fe3d5e944d266e055ba3b03
Guilherme <43280872+guilherme1708@users.noreply.github.com>
2019-05-12 22:04:10 -0300

    Add files via upload

---
+++ lista 5.R
@@ -0,0 +1,45 @@
+# Lista de Processos Estocáticos 
+
+# Bibliotecas
+library(ggplot2)
+
+# Exercício 1
+
+# Exercício 2
+
+c <- 0 # Perder tudo
+d <- 50 # Quanto se quer ganhar
+x <- c(10,20,30,40) # Vetor de valores iniciais
+
+# item a
+p <- 18/38 # probabilidade de vencer a rodada
+q <- 20/38 # 1-p
+
+# item b
+p <- 1/2
+
+# Exercício 3
+
+p0 <- 1/2
+p1 <- 1/4
+p2 <- 1/4
+
+# item a
+
+x0 <- 1
+Nsim <- 100
+
+# Exercício 4
+
+a <- 100 # Quantidade de votos do candidato A
+b <- 50 # Quantidade de votos do candidato B
+Total <- a+b # Total de votos
+
+Nsim <- 1000 # número de simulações
+
+for (i in 1:Nsim){
+  
+}
+
+
+
